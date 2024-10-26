77f187305e113866572eb76ce13dfef8354fe378
guruprasadhns <guruprasadhncse4@gmail.com>
2024-10-26 21:07:15 +0530

    added countby method

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
-export function sayHi(name: string) {
-  console.log("Hi", name);
+export function countBy<T>(
+  array: Array<T>,
+  iteratee: (value: T) => number | string
+): Record<string, number> {
+  const result = array.reduce((acc: Record<string, number>, i: T) => {
+    const key = iteratee(i);
+    acc[key] = (acc[key] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+  return result;
 }
